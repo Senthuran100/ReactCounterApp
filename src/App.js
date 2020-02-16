a342391563f976c2d9dc3af98802242bf0afe50d
Senthuran Manoharan <senthuran.manoharan@gmail.com>
2020-02-16 22:11:26 +0530

    Adding the Repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+ constructor(){
+   super()
+   this.state={
+     count:100,
+   }
+ }
+
+ increment(){
+   this.setState(
+     {
+       count:this.state.count+1
+     }
+   );
+ };
+
+ decrement(){
+   this.setState(
+     {
+    count:this.state.count-1
+     }
+   );
+ };
+
+ reset(){
+   this.setState(
+     {
+       count:0
+     }
+   )
+ }
+
+ render(){
+    return(
+        <div style={{
+          position: 'absolute', left: '50%', top: '50%',
+          transform: 'translate(-50%, -50%)'
+      }}>
+            <h1>Current Count :{this.state.count}</h1>
+            <button className="inc"  onClick={(e)=>this.increment(e)}>Increment!</button>
+            <button  className="dec" onClick={(e)=>this.decrement(e)}>Decrement!</button>
+            <button className="reset" onClick={(e)=>this.reset(e)}>Reset!</button>
+            
+        </div>
+
+    );
+ }
+
+
+
+
 }
 
 export default App;
